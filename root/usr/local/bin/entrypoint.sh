3dd95cc2b88d2935b9d03e0e70fd6cdeaa71b518
Max Mitschke <maxmitschke@fastmail.com>
2024-09-11 22:02:05 -0400

    Update entrypoint.sh

--- root/usr/local/bin/entrypoint.sh
+++ root/usr/local/bin/entrypoint.sh
@@ -3,11 +3,11 @@
 GAME_DATADIR="/data"
 if [[ "$(ls -A ${GAME_DATADIR})" ]]; then
   # Does the $GAME_DATADIR have files within it?
-  GAME_ARGS="-sessionid dedicated -userdir $GAME_DATADIR $GAME_ARGS"
+  GAME_ARGS="-userdir $GAME_DATADIR $GAME_ARGS"
 fi
 
 # This is just to ensure that we are are able to run the service
 # with any user as xonotic needs to have a server.cfg file.
 mkdir -p $HOME/.xonotic/data && touch $HOME/.xonotic/data/server.cfg
 
-/usr/bin/xonotic-dedicated $GAME_ARGS
+/usr/bin/xonotic-dedicated -sessionid dedicated $GAME_ARGS
